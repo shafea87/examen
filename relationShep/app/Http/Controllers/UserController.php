149d4cc9c981337dac16fa4f988319f5eae64cfa
shafea87 <m.s.alsalkini@gmail.com>
2022-01-25 22:59:24 +0100

    end
    end

---
+++ relationShep/app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Phone;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function insertRecord()
+    {
+        $phone = new Phone();
+        $phone->phone = '35342642';
+        $user = new User();
+        $user->name = 'Tom';
+        $user->email = 'tom@ss.com';
+        $user->password = encrypt('secret');
+        $user->save();
+        $user->phone()->save($phone);
+        return'Record has been created';
+    }
+}
